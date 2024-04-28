75c7fda91709754e3bb410f4a784297f18c7f7d0
Chandan Dhumale <142775275+ChandanD1@users.noreply.github.com>
2024-04-28 23:53:08 +0530

    Add files via upload

---
+++ 3-twoInteger.cpp
@@ -0,0 +1,12 @@
+//divide two integers
+ 
+ 
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        int ans=0;
+        ans= dividend/divisor;
+        
+        return ans;
+    }
+};
